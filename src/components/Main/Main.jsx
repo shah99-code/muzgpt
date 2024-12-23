bed845da76bdca45dd7a223eac5a5dab6c4c71fc
Your Name <you@example.com>
2024-12-23 16:44:35 +0500

    ok

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -81,7 +81,7 @@ const Main = () => {
                         </div>
                     </div>
                     <p className="bottom-info">
-                    MuzGPT may not be perfect. Always verify critical info.
+                    MuzGPT may not be perfect. .
                         <a href="#"></a>
                     </p>
                 </div>
