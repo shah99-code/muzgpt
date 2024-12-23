44b58b9653182c08add5ab95c17b8c182b6e4aa1
mushaid shah <mushaidshah@mushaids-MacBook-Pro.local>
2024-12-23 13:22:00 +0500

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,92 @@
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import './Main.css';
+import {assets} from "../../assets/assets.js";
+import {Context} from "../../context/Context.jsx";
+
+const Main = () => {
+    const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context);
+    const resultRef = useRef(null);
+    const [rows, setRows] = useState(1);
+
+    useEffect(() => {
+        const updateRows = () => {
+            if (window.innerWidth <= 600) {
+                setRows(2);
+            } else {
+                setRows(1);
+            }
+        };
+
+        updateRows();
+        window.addEventListener('resize', updateRows);
+        return () => window.removeEventListener('resize', updateRows);
+    }, []);
+
+    useEffect(() => {
+        if (resultRef.current) {
+            resultRef.current.scrollTop = resultRef.current.scrollHeight;
+        }
+    }, [resultData]);
+
+    return (
+        <main className="main">
+            <nav className="nav">
+                <p>MuzGpt</p>
+                <img src={assets.user_icon} alt=""/>
+            </nav>
+            <div className="main-container">
+
+                {!showResult
+                    ? <>
+                        <div className="greet">
+                            <p><span>hello, friend!</span></p>
+                            <p>How can I help you today?</p>
+                        </div>
+                       
+                    </>
+                    :
+                    <div className='result' ref={resultRef}>
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt=""/>
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            <img className="result-data-icon" src={assets.gemini_icon} alt=""/>
+                            {loading ?
+                                <div className='loader'>
+                                    <hr/>
+                                </div>
+                                :
+                                <p dangerouslySetInnerHTML={{__html: resultData}}></p>
+                            }
+                        </div>
+                    </div>
+                }
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <textarea rows={rows} onChange={(e) => setInput(e.target.value)}
+                                  onKeyUp={(e) => {
+                                      if (e.key === 'Enter') {
+                                          onSent();
+                                      }
+                                  }}
+                                  value={input}
+                                  type="text"
+                                 placeholder="Ask MuzGpt"
+                        />
+                        <div className="icon-container">
+                           
+                            <button type="submit" onClick={() => onSent()}><img src={assets.send_icon} alt=""/></button>
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                        Muzgpt may display inaccurate info, including about people, so double-check its responses.
+                        <a href="#"></a>
+                    </p>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Main;
